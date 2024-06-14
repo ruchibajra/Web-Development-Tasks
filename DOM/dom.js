7850a1bfc250fead039607222d060544ccd55f35
ruchibajra <ruchibajracharya8@gmail.com>
2024-06-14 21:12:32 +0545

    search bar added

--- DOM/dom.js
+++ DOM/dom.js
@@ -13,3 +13,24 @@ function addBook(){
         ul.appendChild(li);
     }
 }
+
+/*Question 2: Book Search Functionality
+Implement a function searchBook() that highlights books in the list matching the search term
+from the input field with the ID search-bar, setting their background color to yellow. */
+
+function searchBook(){
+    const searchTerm = document.getElementById('search-bar').value.toLocaleLowerCase();
+    const books = document.querySelectorAll('#book-list li');
+
+    for(i=0; i<books.length; i++){
+        const book = books[i];
+
+        if(book.textContent.toLocaleLowerCase().includes(searchTerm)){
+            book.classList.add('highlight');
+        }else
+        {
+            book.classList.remove('highlight');
+        }
+    }
+
+}
