d298c4738374e05ec13c36b038254754625ed027
ruchibajra <ruchibajracharya8@gmail.com>
2024-06-14 20:29:36 +0545

    addBook complete

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,3 +1,7 @@
+/*Question 1: Dynamic Book Addition
+Write a function addBook() that prompts the user for a book title and appends it as a new li
+element to the ul with the ID book-list.*/
+
 function addBook(){
     const bookTitle = prompt("Enter Book Title");
 
