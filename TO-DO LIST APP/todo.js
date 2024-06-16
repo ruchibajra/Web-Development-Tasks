935c89e26bae0b9a74e4b6c43adb581ed4892a1a
ruchibajra <ruchibajracharya8@gmail.com>
2024-06-16 14:22:27 +0545

    todo data save after browser load

--- TO-DO LIST APP/todo.js
+++ TO-DO LIST APP/todo.js
@@ -13,19 +13,33 @@ function addTask() {
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
-
   }
-  inputBox.value ='';
+  inputBox.value = "";
+  saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked");
+// for check uncheck and remove li text
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
+  },
+  false
+);
 
-    else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove();
-    }
-}, false);
+// to store and save data
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
 
-
+// to show the saved data even after loading the browser
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
