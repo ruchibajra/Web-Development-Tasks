255563c969006042708ba55018348bf1dc2aa82a
ruchibajra <ruchibajracharya8@gmail.com>
2024-06-17 15:44:40 +0545

    mouse over and mouse out

--- EVENT HANDLING/event.js
+++ EVENT HANDLING/event.js
@@ -1,6 +1,6 @@
 // EVENT HANDLING
 
-// ONCLICK
+// ON CLICK
 let btnClick = document.getElementById("btn-click");
 let demoPara = document.getElementById("demo");
 
@@ -9,7 +9,7 @@ btnClick.addEventListener("click", () => {
 });
 
 
-// ONCHANGE
+// ON CHANGE
 let onchangeSelect = document.getElementById("onchange-select");
 let onchangeText = document.getElementById("onchange-text");
 
@@ -17,6 +17,22 @@ onchangeSelect.addEventListener("change", () => {
   let selectedValue = onchangeSelect.value;
   onchangeText.innerHTML =  'the selected option is : ' + selectedValue;
 });
+
+
+// ON MOUSE OVER AND MOUSE OUT 
+let mouseOnText = document.getElementById('mouse-on-text');
+
+mouseOnText.addEventListener('mouseover', mouse_over_fn);
+mouseOnText.addEventListener('mouseout', mouse_out_fn);
+
+function mouse_over_fn(){
+    mouseOnText.style.color = 'red';
+}
+
+function mouse_out_fn(){
+    mouseOnText.style.color = 'black';
+}
+
 
 
 
